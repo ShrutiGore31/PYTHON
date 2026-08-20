93e572b46f41a9fc4c687ec01b52648a80354a30
Shruti Gore <shrutigore.in@gmail.com>
2026-08-20 18:12:10 +0530

    Add que71 Python Practices

---
+++ que71.py
@@ -0,0 +1,20 @@
+#Write a program which accept one number and display below pattern.
+#Input : 5
+#Output :   1    2    3    4    5
+#           1    2    3    4    5
+#           1    2    3    4    5
+#           1    2    3    4    5
+#           1    2    3    4    5
+
+def DisplayPattern(No):
+    for i in range(1, No+1):
+        for j in range(1, No+1):
+            print(j,end ="\t")
+        print()
+
+def main():
+    value= int(input("Enter number : "))
+    DisplayPattern(value)
+
+if __name__=="__main__":
+    main()                
