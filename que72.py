0520c097c25faa7d62c272403abc4a6716419a14
Shruti Gore <shrutigore.in@gmail.com>
2026-08-20 18:13:39 +0530

    Add que72 Python Practices

---
+++ que72.py
@@ -0,0 +1,27 @@
+#8. Write a program which accept one number and display below pattern.
+#Input : 5
+#Output :
+'''
+1
+1   2
+1   2   3
+1   2   3   4
+1   2   3   4   5
+
+'''
+#####################################################################################################
+
+
+def PatternPrinting(No):
+    for i in range(1,No+1):
+        for j in range(1, i+1):
+            print(j, end = "\t")
+        print()    
+
+def main():
+    value=int(input("Enter Number: "))
+    PatternPrinting(value)
+
+
+if __name__=="__main__":
+    main()                
